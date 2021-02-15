8917d978d589973e879760e778dc5f35365d9fa1
kevin-shitaye <it.kevin.shitaye@gmail.com>
2021-02-15 22:13:53 +0300

    made the the different kinds of inheritance

--- asset/js/05(OOP4).js
+++ asset/js/05(OOP4).js
@@ -2,6 +2,10 @@
 function Person(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
+
+    Person.prototype.getFullName = function () {
+        return  this.firstName + " " + this.lastName
+    }
 }
 
 // 1. Create a getFullName Method on prototype object 
@@ -13,22 +17,22 @@ function Customer(firstName, lastName, membership) {
 
     //3. Call the Person Object Constructor using call [Person.call(this,firsName,lastName)]
 
-
+    Person.call(this,firstName,lastName)
     this.membership = membership;
 
 }
 
 // 4. Set the prototype of Customer to be Person Prototype 
 // Note : Use Object.create
-
+Customer.prototype = Object.create(Person.prototype)
 
 //5. Change the constructor of Customer to be Customer 
 
-
+Customer.prototype.constructor = Customer;
 
 // Create Customer Object pass ["Jorah", "Mormont", "PRO"]
-
+customer1 = new Customer("Jorah", "Mormont", "PRO")
 
 //6. Display The Full Name [remove the string when you have the object]
 
-fun_proto.innerHTML = "custemr_1.getFullName()";
+fun_proto.innerHTML = customer1.getFullName();
