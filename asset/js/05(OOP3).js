82e63e2b6a15f4a51e2d1d81948237fe0a611a9d
kevin-shitaye <it.kevin.shitaye@gmail.com>
2021-02-15 21:55:56 +0300

    made the the es6 class

--- asset/js/05(OOP3).js
+++ asset/js/05(OOP3).js
@@ -1,5 +1,14 @@
 // 1. Create a Person_ES6 Class 
+class Person_ES6{
+    constructor(firstname, lastName){
+        this.fn = firstname
+        this.ln = lastName
+    }
 
+    greeting(){
+        return `Hi ${this.fn} ${this.ln}`
+    }
+}
 
 
 //2. Create the constructor that initialize firstName and lastName
@@ -10,7 +19,7 @@
 
 //4. Create  Object using ES6 class by passing firstName,lastName ["Bran Stark"]
 
-
+let Bran = new Person_ES6("Bran", "Stark")
 
 //5. Display the greeting on Bran Object [remove the string when you have the object]
-Obj_C3.innerHTML = "Bran.greeting()";
+Obj_C3.innerHTML = Bran.greeting();
