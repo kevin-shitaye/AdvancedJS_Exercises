9859d86aa350c816a930a14996f4426c7a50170d
kevin-shitaye <it.kevin.shitaye@gmail.com>
2021-02-15 11:41:44 +0300

    finished object destructuring.

--- asset/js/02(newLoops).js
+++ asset/js/02(newLoops).js
@@ -13,7 +13,11 @@ function forINLoop() {
     const student = { name: "Abebe", age: 21, year: "2dn year" }
 
     // 1. Create a for in loop that iterate over the student object
-
+    for (const key in student) {
+         forInLoop.innerHTML += `<li class="list-group-item" >${key} : ${student[key]}</li>`;
+    
+        }
+    
 
     // 2. Inside the loop paint the UI [Use the Format Given Below]
 
@@ -35,7 +39,9 @@ function forOFLoop() {
     const students = [{ name: "Abebe", age: 21, year: "2dn year" }, { name: "Kebede", age: 22, year: "2dn year" }, { name: "Abebech", age: 23, year: "2dn year" }]
 
     // 1. Create a for of loop that iterate over the students Array and displays the names of students 
-
+    for (const student of students) {
+        forOfLoop.innerHTML +=`<li class="list-group-item" >Name : ${student.name}</li>`;
+    }
 
     // 2. Inside the loop paint the UI [Use the Format Given Below]
 
