f38b4c53e3b9076a8df9d1849601b8eac934d72c
kevin-shitaye <it.kevin.shitaye@gmail.com>
2021-02-15 10:18:25 +0300

    finished the Symbol question.

--- asset/js/01(Symbol).js
+++ asset/js/01(Symbol).js
@@ -15,25 +15,26 @@ document.addEventListener('DOMContentLoaded', () => {
 function displaySymbol() {
 
     // 1. Create an id constant that store Symbol with id debugging string 
+    const id = Symbol('debugging string')
 
     //2. Display the symbol on ui as string using toString()
-    symbolResult.innerHTML = "id.toString()";
+    symbolResult.innerHTML = id.toString();
 
 }
 
 function displaySymbolEquality() {
 
     // 1. Create an id1 constant that store Symbol with id debugging string 
-
+    const id1 = Symbol("debug string2")
 
     // 2. Create an id2 constant that store Symbol with id debugging string 
-
+    const id2 = Symbol('debug string2')
 
     // 3. Compare their equality (===) , store it on result variable
-
+    result = id1 === id2
 
     //2. Display the result variable [Remove the string once you have the variable]
-    symbolEquality.innerHTML = "result";
+    symbolEquality.innerHTML = result;
 
 
 
@@ -45,9 +46,11 @@ function displaySymbolObject() {
     const student = { name: "Abebe", age: 21, year: "2dn year" }
 
     // 1. Create an id constant that store Symbol with id debugging string 
+    const id = Symbol("1234")
 
 
     //2. Add the symbol property to the object [this will be considered as hidden property]
+    student['id'] = id
     //3. Let the Id value be 1234
     // Note : user [] 
 
@@ -55,6 +58,6 @@ function displaySymbolObject() {
     //Display the object on console 
     console.log(student);
 
-    symbolObject.innerHTML = "View it On Console";
+    symbolObject.innerHTML = student['id'].toString();
 
 }
